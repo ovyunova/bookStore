e0745bf6bdad4590ff5eb6a0b8c33b30a4d02f2e
VyunovaO <Olga.vyunova@gmail.com>
2018-12-07 18:50:51 +0300

    Сменила название класса у блока новинок

--- js/modules/booksCardTemplate.js
+++ js/modules/booksCardTemplate.js
@@ -3,14 +3,14 @@ const bookCardTemplate = {
   tag: 'article',
   tagClass: 'products-card-mini',
   setContent: function(data) {
-    return `<h2 class="products-card-mini__title">
+    return `<h2 class="product-card-mini__title">
             <a href="${data.url}">${data.name}</a>
           </h2>
-          <a href="${data.url}" class="products-card-mini__img-wrap">
+          <a href="${data.url}" class="product-card-mini__img-wrap">
             <img src="#" class="products-card-mini__img">
           </a>
-          <p class="products-card-mini__descr">${data.desc} </p>
-          <div class="products-card-mini__price">${data.price} &#8381</div>`;
+          <p class="product-card-mini__descr">${data.desc} </p>
+          <div class="product-card-mini__price">${data.price} &#8381</div>`;
   }
 }
 
