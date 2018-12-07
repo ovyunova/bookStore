4e5a1936a5b90588754e2409af50e6fab28c9249
VyunovaO <Olga.vyunova@gmail.com>
2018-12-07 15:47:07 +0300

    Внесла правки по комментариям-2

--- js/modules/createElement.js
+++ js/modules/createElement.js
@@ -1,9 +1,9 @@
 function createElement (book, template) {
-    const article = document.createElement(template.tag);
-    article.classList.add(template.tagClass);
-    article.innerHTML = template.setContent(book);
+  const article = document.createElement(template.tag);
+  article.classList.add(template.tagClass);
+  article.innerHTML = template.setContent(book);
 
-    return article;
+  return article;
 };
 
 export default createElement;
