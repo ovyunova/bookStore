fd83c207ab72a97ee4406956fa74a3ea061e055f
VyunovaO <Olga.vyunova@gmail.com>
2018-12-09 19:55:45 +0300

    Вынесла запрос к серверу в отдельный модуль

--- js/modules/booksCardTemplate.js
+++ js/modules/booksCardTemplate.js
@@ -4,10 +4,10 @@ const bookCardTemplate = {
   tagClass: 'products-card-mini',
   setContent: function(data) {
     return `<h2 class="product-card-mini__title">
-            <a href="${data.url}">${data.name}</a>
+            <a href="${data.uri}">${data.name}</a>
           </h2>
-          <a href="${data.url}" class="product-card-mini__img-wrap">
-            <img src="#" class="products-card-mini__img">
+          <a href="${data.uri}" class="product-card-mini__img-wrap">
+            <img src="img/books_all/${data.uri}.png" class="products-card-mini__img">
           </a>
           <p class="product-card-mini__descr">${data.desc} </p>
           <div class="product-card-mini__price">${data.price} &#8381</div>`;
---
+++ js/modules/sendRequest.js
@@ -0,0 +1,19 @@
+function sendRequest(data, onSuccess) {
+  let xhr = new XMLHttpRequest;
+
+  xhr.open('GET', data);
+  xhr.send();
+
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+
+      const responseObj = JSON.parse(xhr.responseText);
+      onSuccess(responseObj);
+
+    } else {
+      console.log(`жду`+ xhr.readyState);
+    }
+  };
+};
+
+export default sendRequest;
