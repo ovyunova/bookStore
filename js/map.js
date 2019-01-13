3ef83d8d4bbe3f0e79535872dc2673749f798cc9
VyunovaO <Olga.vyunova@gmail.com>
2019-01-13 12:57:05 +0300

    Updates

---
+++ js/map.js
@@ -0,0 +1,14 @@
+// initMap() - функция инициализации карты
+function initMap() {
+// Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
+var centerLatLng = new google.maps.LatLng(59.916785, 30.280604);
+// Обязательные опции с которыми будет проинициализированна карта
+var mapOptions = {
+center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
+zoom: 17               // Зум по умолчанию. Возможные значения от 0 до 21
+};
+// Создаем карту внутри элемента #map
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+// Ждем полной загрузки страницы, после этого запускаем initMap()
+google.maps.event.addDomListener(window, "load", initMap);
