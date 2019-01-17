6d1e393e371a258621e151534c070c531c93584b
VyunovaO <Olga.vyunova@gmail.com>
2019-01-17 23:22:12 +0300

    Добавила стилизацию блока продуктов на главной

--- src/js/modules/booksCardTemplate.js
+++ src/js/modules/booksCardTemplate.js
@@ -1,13 +1,13 @@
 const bookCardTemplate = {
   wrap: '.j-cards',
   tag: 'article',
-  tagClass: 'products-card-mini',
+  tagClass: 'product-card-mini',
   setContent: function(data) {
     return `<h2 class="product-card-mini__title">
             <a href="${data.uri}">${data.name}</a>
           </h2>
           <a href="${data.uri}" class="product-card-mini__img-wrap">
-            <img src="img/books_all/${data.uri}.png" class="products-card-mini__img">
+            <img src="img/books_all/${data.uri}.png" class="product-card-mini__img">
           </a>
           <p class="product-card-mini__descr">${data.desc} </p>
           <div class="product-card-mini__price">${data.price} &#8381</div>`;
